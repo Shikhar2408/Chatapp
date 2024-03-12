bda61901ded6649c09c9a09ed361993804a308c5
Shikhar2408 <shikharkumar2408@gmail.com>
2024-03-13 01:37:16 +0530

    Source code

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async(req, res) => {
+    const { username } = req.body;
+    return res.json({ username: username, secret: "bajrang" });
+});
+
+app.listen(3000);
